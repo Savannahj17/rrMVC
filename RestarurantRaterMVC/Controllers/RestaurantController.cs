2925a184d2ac9dfd6c4f4144a609a17fb5299d85
Savannah Jackson <savannahjackson33@gmail.com>
2022-01-12 16:52:47 -0500

    EditFunctionality

--- RestarurantRaterMVC/Controllers/RestaurantController.cs
+++ RestarurantRaterMVC/Controllers/RestaurantController.cs
@@ -65,5 +65,36 @@ namespace RestarurantRaterMVC.Controllers
             return RedirectToAction("Index");
         }
 
+        //GET: Restaurant/Edit/{id}
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
     }
 }
